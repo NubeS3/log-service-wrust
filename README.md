ae93a10a3e3d87ea0eb3b2c7c02df9d5ca7131dc
Tuong Nguyen <tuongn09@gmail.com>
2021-03-23 11:56:56 +0700

    split to .env

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # log-service-wrust
-A logging service storing into Cassandra DB with Rust.
+A logging service storing into Cassandra DB write in Rust.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
+use nats;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    connect_nats();
+}
+
+fn connect_nats() {
+    dotenv().ok();
+
+    let nats_url = match env::var("NATS_ENDPOINT") {
+        Ok(url) => url,
+        Err(e) => panic!("{}", e),
+    };
+
+    let nats_subj = match env::var("NATS_SUBJECT") {
+        Ok(subj) => subj,
+        Err(e) => panic!("{}", e),
+    };
+
+    let nc = nats::connect(&nats_url);
+    let sub = nc.subscribe(&nats_subj);
 }
